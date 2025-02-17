37ba1784cb3cee0faae7ef59d439479deec82861
ochaoo <oleksii.chalenko@nure.ua>
2025-02-17 11:43:59 +0200

    update AR

--- src/BackpackConfigurator.jsx
+++ src/BackpackConfigurator.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Environment } from "@react-three/drei";
-import { ARButton, XR, Controllers, Hands } from "@react-three/xr";
+import { XR, createXRStore } from "@react-three/xr";
 import { QRCodeSVG } from "qrcode.react";
 import BackpackModel from "./components/models/BackPackModel";
 import Icon from "/button_icon.svg?url";
 
 const colors = ["#8B4512", "black", "blue"];
 const metals = ["silver", "black", "gold"];
+
+const xrStore = createXRStore();
 
 const BackpackConfigurator = () => {
   const [color, setColor] = useState("#8B4512");
@@ -17,11 +19,19 @@ const BackpackConfigurator = () => {
 
   const isMobile = /Mobi|Android|iPhone/i.test(navigator.userAgent);
 
+  const handleAR = () => {
+    if (isMobile) {
+      xrStore.enterXR();
+    } else {
+      setShowQR(true);
+    }
+  };
+
   return (
     <div>
       <div className="flex justify-center pt-[35px] font-medium text-sm text-white">
         <button
-          onClick={() => setShowQR(!showQR)}
+          onClick={handleAR}
           className="w-[156px] h-10 flex justify-center items-center gap-2.5 bg-[#4169E1] rounded-4xl hover:cursor-pointer hover:bg-[#758fdd]"
         >
           <img src={Icon} /> See In Real Life
@@ -30,47 +40,34 @@ const BackpackConfigurator = () => {
 
       <div className="w-screen h-[60vh] flex justify-center items-center">
         {showQR ? (
-          isMobile ? (
-            <div className="w-full h-full flex flex-col items-center">
-              <ARButton sessionInit={{ requiredFeatures: ["hit-test"] }} />
-              <Canvas>
-                <XR>
-                  <ambientLight intensity={0.5} />
-                  <directionalLight position={[0, 5, 5]} intensity={1} />
-                  <BackpackModel position={[0, 0, -2]} metalColor={metal} bodyColor={color} materialType={material} />
-                  <Controllers />
-                  <Hands />
-                </XR>
-              </Canvas>
+          <div className="w-[305px] h-[400px] border-[1px] rounded-4xl border-[#4169E1] flex flex-col items-center text-[#4169E1] pt-[16px]">
+            <button className="hover:cursor-pointer self-end mr-5" onClick={() => setShowQR(!showQR)}>
+              X
+            </button>
+            <span className="w-[225px] mb-[30px] border-b-[1px] border-[#4169E1] pb-1 text-center text-lg">
+              Scan the QR code with your phone. Within 1-3 seconds the AR function opens on your phone.
+            </span>
+            <div className="w-[190px] h-[190px] border-[1px] rounded-4xl border-[#4169E1] flex justify-center items-center">
+              <QRCodeSVG
+                value="https://test-marevo-three.vercel.app/"
+                size={128}
+                fgColor="#4169E1"
+                bgColor="transparent"
+              />
             </div>
-          ) : (
-            <div className="w-[305px] h-[400px] border-[1px] rounded-4xl border-[#4169E1] flex flex-col items-center text-[#4169E1] pt-[16px]">
-              <button className="hover:cursor-pointer self-end mr-5" onClick={() => setShowQR(!showQR)}>
-                X
-              </button>
-              <span className="w-[225px] mb-[30px] border-b-[1px] border-[#4169E1] pb-1 text-center text-lg">
-                Scan the QR code with your phone. Within 1-3 seconds the AR function opens on your phone.
-              </span>
-              <div className="w-[190px] h-[190px] border-[1px] rounded-4xl border-[#4169E1] flex justify-center items-center">
-                <QRCodeSVG
-                  value="https://test-marevo-three.vercel.app/"
-                  size={128}
-                  fgColor="#4169E1"
-                  bgColor="transparent"
-                />
-              </div>
-            </div>
-          )
+          </div>
         ) : (
-          <Canvas camera={{ position: [0, 0, 3] }}>
-            <OrbitControls />
-            <Environment preset="apartment" />
+          <XR store={xrStore} sessionInit={{ optionalFeatures: ["local-floor", "bounded-floor"] }}>
+            <Canvas camera={{ position: [0, 0, 3] }}>
+              <OrbitControls />
+              <Environment preset="apartment" />
 
-            <directionalLight position={[0, 2, 3]} intensity={2} castShadow />
-            <ambientLight intensity={0.8} />
+              <directionalLight position={[0, 2, 3]} intensity={2} castShadow />
+              <ambientLight intensity={0.8} />
 
-            <BackpackModel metalColor={metal} bodyColor={color} materialType={material} />
-          </Canvas>
+              <BackpackModel metalColor={metal} bodyColor={color} materialType={material} />
+            </Canvas>
+          </XR>
         )}
       </div>
 
